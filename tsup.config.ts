4f532dad9743b7249c416f698cc7e25d827f2fc5
BlankParticle <blankparticle@gmail.com>
2024-03-25 10:27:23 +0530

    ci: embrace chaos

--- tsup.config.ts
+++ tsup.config.ts
@@ -27,7 +27,11 @@ export default defineConfig({
     ].join("\n"),
   },
   onSuccess: async () => {
+    if (process.argv.includes("--no-pack")) return;
     await fs.copy("./assets", "./dist");
-    await $`cd dist && glib-compile-schemas schemas && zip -r ./shell-extension.zip . -9r`;
+    const files = (await fs.readdir("./dist"))
+      .filter((f) => fs.statSync(`./dist/${f}`).isFile())
+      .map((file) => `--extra-source=./${file}`);
+    await $`cd dist && gnome-extensions pack -f ${files} -o ./`;
   },
 });
